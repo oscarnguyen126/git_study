bea57254b13744dd8daefb378a440902d33ab1ce
trau dat <traudat@gmail.com>
2022-08-08 23:32:12 +0700

    Feature: login user

--- model.py
+++ model.py
@@ -2,6 +2,11 @@ from unicodedata import name
 
 
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
+        self.password = password
+
+    def validate_password(self, password):
+        return self.password == password
+    
